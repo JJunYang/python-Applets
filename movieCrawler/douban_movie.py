f3d711a3dadbf809e1a9b188a92fff44cfffea89
junyang <yang.jun3@husky.neu.edu>
2020-08-17 16:23:38 -0400

    speed up with multi thread

--- movieCrawler/douban_movie.py
+++ movieCrawler/douban_movie.py
@@ -1,6 +1,8 @@
 import os
 import requests
 from bs4 import BeautifulSoup
+import time
+from concurrent.futures import ThreadPoolExecutor, wait, ALL_COMPLETED
 
 download_path = './douban'
 if not os.path.exists(download_path):
@@ -17,8 +19,8 @@ def download_pic(url):
     pic_link_list = [image['src'] for image in images]
     pic_name_list = [image['alt'] for image in images]
     for name, link in zip(pic_name_list, pic_link_list):
-        html=requests.get(link)
-        with open(f'{download_path}/{name}.jpg','wb') as f:
+        html = requests.get(link)
+        with open(f'{download_path}/{name}.jpg', 'wb') as f:
             f.write(html.content)
     print(f'{url} download finished')
 
@@ -30,8 +32,21 @@ def main():
         start_urls.append(
             f'https://movie.douban.com/top250?start={25 * i}&filter=')
 
-    for url in start_urls:
-        download_pic(url)
+    start_time = time.time()
+
+    # for url in start_urls:
+    #     download_pic(url)
+
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        futures = []
+        for url in start_urls:
+            future = executor.submit(download_pic, url)
+            futures.append(future)
+    wait(futures, return_when=ALL_COMPLETED)
+
+    end_time = time.time()
+    print('='*50)
+    print(f'total runtime:{end_time-start_time}')
 
 
 if __name__ == "__main__":
